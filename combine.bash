5fba84e35a4822f9bf1cf96262955baec857e7e0
Andri Pétur <andripeturth@gmail.com>
2016-09-08 16:21:53 +0200

    File restructuring, and added combine script

---
+++ combine.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SOURCES=('libs/polyfills.js'
+'defaults.js'
+'libs/fabric.min.js'
+'draw.js'
+'libs/flocking-all.min.js'
+'libs/jqconsole.min.js'
+'console.js'
+'utilities.js'
+'schillinger_functions.js'
+'chaos.js'
+'synthDefinitions.js'
+'composition.js'
+'help.js' )
+
+# clear destination file
+> oregla.js
+
+for i in ${SOURCES[@]}; do
+  file='source/'${i}
+  uglifyjs $file >> oregla.js || cat $file >> oregla.js #minify file, if fail, just append it
+done
+
+
+echo 'done'
